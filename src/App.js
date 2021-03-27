d6698e06a5929dbe528c084352aac0a679e0b15b
prakharpbuf <prakhar.paw08@gmail.com>
2021-03-26 22:12:00 -0400

    meme generator

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "./Header"
+import MemeGenerator from "./MemeGenerator"
+import "./styles.css"
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <MemeGenerator />
+        </div>
+    )
+}
+
+export default App
